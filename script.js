b41cbeee1be95e1f57dd0a4248b8680977ba0f7b
Arthur Prado <arthur.matheus.prado@gmail.com>
2022-09-26 22:30:07 -0300

    Exercícios de Fixação

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById(`fruta-1`)
+fruta1.innerHTML = frutas[0]
+
+const fruta2 = document.getElementById(`fruta-2`)
+fruta2.innerHTML = frutas[1]
+
+const fruta3 = document.getElementById(`fruta-3`)
+fruta3.innerHTML = frutas[2]
+
+const novaFruta = document.getElementById("inputFruta")
+
+const fruta4 = document.getElementById("fruta-4")
+function changeFruit(){
+    fruta4.innerHTML = novaFruta.value 
+}
